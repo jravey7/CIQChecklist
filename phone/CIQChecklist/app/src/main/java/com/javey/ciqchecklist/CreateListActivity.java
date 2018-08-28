d34764f90c55a5cc39ec23222e438633f1a42910
Joseph Avey <joseph.avey@garmin.com>
2018-08-28 13:21:22 -0500

    created ListView for create list activity (app crashing on start create list)
    Change-Id: Idcad677c20a53b64940e6327abddf5578b8a845c

--- phone/CIQChecklist/app/src/main/java/com/javey/ciqchecklist/CreateListActivity.java
+++ phone/CIQChecklist/app/src/main/java/com/javey/ciqchecklist/CreateListActivity.java
@@ -4,27 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class CreateListActivity extends AppCompatActivity {
 
     private Integer addCount = 0;
     private Integer doneCount = 0;
 
+    String[] defaultListItems = {"Onions", "Peppers", "Chicken", "Salad dressing", "Eggs"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_list);
 
+        // not used atm
         Intent intent = getIntent();
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.activity_create_list, defaultListItems);
+
+        ListView listView = findViewById(R.id.checklistItems);
+        listView.setAdapter(adapter);
     }
 
     public void onClickAddListItem(View view)
     {
         this.addCount++;
-        EditText editTextItem = findViewById(R.id.editListItem);
-        String displayText = "Add count: " + addCount;
-        editTextItem.setText(displayText);
+        //EditText editTextItem = findViewById(R.id.editListItem);
+        //String displayText = "Add count: " + addCount;
+        //editTextItem.setText(displayText);
+
+
     }
 
     public void onClickDoneCreateList(View view)
