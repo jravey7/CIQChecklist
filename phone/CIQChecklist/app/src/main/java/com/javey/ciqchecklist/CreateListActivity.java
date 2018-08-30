13421e9ddb63372187406cb41fcf641e5245e27e
Joe Avey <javey@iastate.edu>
2018-08-29 22:50:18 -0500

    Create List activity can now add/edit/remove list items

--- phone/CIQChecklist/app/src/main/java/com/javey/ciqchecklist/CreateListActivity.java
+++ phone/CIQChecklist/app/src/main/java/com/javey/ciqchecklist/CreateListActivity.java
@@ -1,19 +1,27 @@
 package com.javey.ciqchecklist;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class CreateListActivity extends AppCompatActivity {
 
-    private Integer addCount = 0;
-    private Integer doneCount = 0;
+    String[] defaultListItems = {"List Item #1"};
 
-    String[] defaultListItems = {"Onions", "Peppers", "Chicken", "Salad dressing", "Eggs", "Onions", "Peppers", "Chicken", "Salad dressing", "Eggs", "Onions", "Peppers", "Chicken", "Salad dressing", "Eggs"};
+    ArrayList<String> listItems = new ArrayList<>();
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,27 +31,118 @@ public class CreateListActivity extends AppCompatActivity {
         // not used atm
         Intent intent = getIntent();
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, defaultListItems);
+        Collections.addAll(listItems, defaultListItems);
+//        for(int i = 0; i < defaultListItems.length; i++)
+//        {
+//            listItems.add(defaultListItems[i]);
+//        }
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
 
         ListView listView = findViewById(R.id.checklistItems);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(listItemClickedListener);
+        listView.setOnItemLongClickListener(listItemLongClickedListener);
     }
 
     public void onClickAddListItem(View view)
     {
-        this.addCount++;
-        //EditText editTextItem = findViewById(R.id.editListItem);
-        //String displayText = "Add count: " + addCount;
-        //editTextItem.setText(displayText);
+        // create a dialog to ask the user for the list item's label
+        AlertDialog.Builder textInputDialog = new AlertDialog.Builder(this);
+        textInputDialog.setTitle("Add list item");
 
+        // add an editable view to the dialog to accept user input
+        final EditText textInput = new EditText(this );
+        textInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        textInputDialog.setView(textInput);
 
+        textInputDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       // add user input to the list of items
+                       listItems.add(textInput.getText().toString());
+                       adapter.notifyDataSetChanged();
+                   }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // user cancelled
+                    }
+                });
+
+        textInputDialog.show();
     }
 
     public void onClickDoneCreateList(View view)
     {
-        this.doneCount++;
-        EditText editListName = findViewById(R.id.editListName);
-        String displayText = "Done count: " + doneCount;
-        editListName.setText(displayText);
+
     }
+
+    // implement OnItemClick() in the adapter listener class for editing a list item when it's short clicked
+    private AdapterView.OnItemClickListener listItemClickedListener = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView parent, View v, int pos, long id)
+        {
+            // create a dialog to ask the user for the list item's label
+            AlertDialog.Builder textInputDialog = new AlertDialog.Builder(parent.getContext());
+            textInputDialog.setTitle("Edit list item");
+
+            // add an editable view to the dialog to accept user input
+            final EditText textInput = new EditText(parent.getContext() );
+            textInput.setInputType(InputType.TYPE_CLASS_TEXT);
+            textInput.setText(listItems.get(pos));
+            textInputDialog.setView(textInput);
+
+            final int constPos = pos;
+
+            textInputDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    listItems.set(constPos, textInput.getText().toString());
+                    adapter.notifyDataSetChanged();
+                }
+            })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // user cancelled
+                        }
+                    });
+
+            textInputDialog.show();
+        }
+    };
+
+    // implement OnItemClick() in the adapter listener class for editing a list item when it's long clicked
+    private AdapterView.OnItemLongClickListener listItemLongClickedListener = new AdapterView.OnItemLongClickListener() {
+
+        boolean confirmedDelete = false;
+
+        @Override
+        public boolean onItemLongClick(AdapterView parent, View v, int pos, long id)
+        {
+            // create a dialog to ask the user for the list item's label
+            AlertDialog.Builder confirmDialog = new AlertDialog.Builder(parent.getContext());
+            confirmDialog.setTitle("Remove list item?")
+                    .setMessage(listItems.get(pos));
+
+            final int constPos = pos;
+
+            confirmDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // user confirmed to delete this list item
+                    confirmedDelete = true;
+                    listItems.remove(constPos);
+                    adapter.notifyDataSetChanged();
+                }
+            })
+            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // user cancelled the delete
+                    confirmedDelete = false;
+                }
+            });
+            confirmDialog.show();
+
+            return true;
+        }
+    };
 }
